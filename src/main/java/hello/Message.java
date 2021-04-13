9142eaebeac43547f788ed1dae97880ce4baf89e
vivekkulkarni68 <81356991+vivekkulkarni68@users.noreply.github.com>
2021-04-13 20:31:49 +0530

    first change

--- src/main/java/hello/Message.java
+++ src/main/java/hello/Message.java
@@ -11,7 +11,7 @@ public class Message {
     }
     private static final Message msg = new Message();
      Message(){
-    	 details="hi there";
+    	 details="hi there !!!";
      }
      public void setMessage(String str){
     	System.out.println("DEBUG : setMessage is called");
