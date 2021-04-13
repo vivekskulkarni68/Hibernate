22cb715689408fffca34eb425844a4a50372128c
vivekkulkarni68 <81356991+vivekkulkarni68@users.noreply.github.com>
2021-04-13 19:17:55 +0530

    2 changes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,8 @@
 
 public class HelloWorld {
-
+ //Adding main changed #1
+  public static void main(string[] args){
+     System.out.println("DEBUG : hello");
+     System.out.println("DEBUG : hello 2");
+  }
 }
